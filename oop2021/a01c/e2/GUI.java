7634783785566bb8befe8b548790b50cf036dfc3
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-01-16 16:02:53 +0100

    made a01c GUI (e2)

--- oop2021/a01c/e2/GUI.java
+++ oop2021/a01c/e2/GUI.java
@@ -2,16 +2,16 @@ package a01c.e2;
 
 import javax.swing.*;
 import java.util.*;
-import java.util.List;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class GUI extends JFrame {
     
     private static final long serialVersionUID = -6218820567019985015L;
-    private final List<JButton> cells = new ArrayList<>();
-    
+    private final Map<JButton,Pair<Integer,Integer>>cells = new HashMap<>();
+    private final Logics logics;
     public GUI(int size) {
+        this.logics = new LogicsImpl();
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(50*size, 50*size);
         
@@ -20,14 +20,22 @@ public class GUI extends JFrame {
         
         ActionListener al = e -> {
         	var button = (JButton)e.getSource();
-        	button.setText(""+cells.indexOf(button));
-        	button.setEnabled(false); 
+        	var position = cells.get(button);
+            var result = logics.hit(position.getX(), position.getY());
+
+            if(result){
+                for(var entry: cells.entrySet()){
+                    if(logics.isSelected(entry.getValue().getX(), entry.getValue().getY())){
+                        entry.getKey().setText("*");
+                    }
+                }
+            }
         };
                 
         for (int i=0; i<size; i++){
             for (int j=0; j<size; j++){
                 final JButton jb = new JButton(" ");
-                this.cells.add(jb);
+                this.cells.put(jb, new Pair<>(j, i));
                 jb.addActionListener(al);
                 panel.add(jb);
             }
