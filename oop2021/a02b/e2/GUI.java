c0fd73aed2602ca86df1d2d8d9e0ee1b6969e148
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-01-17 11:37:49 +0100

    made the GUI

--- oop2021/a02b/e2/GUI.java
+++ oop2021/a02b/e2/GUI.java
@@ -9,10 +9,11 @@ import java.awt.event.ActionListener;
 public class GUI extends JFrame {
     
     private static final long serialVersionUID = -6218820567019985015L;
-    private final List<JButton> cells = new ArrayList<>();
-    private int counter = 0;
+    private final Map<Pair<Integer, Integer>, JButton> cells = new HashMap<>();
+    private final Logics logics;
     
     public GUI(int size) {
+        this.logics = new LogicsImpl(size);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(50*size, 50*size);
         
@@ -20,19 +21,33 @@ public class GUI extends JFrame {
         this.getContentPane().add(panel);
         
         ActionListener al = e -> {
-        	this.cells.get(counter).setText(String.valueOf(counter++));
+        	logics.toNext();
+            if(logics.isOver()){
+                System.exit(0);
+            }else{
+                this.updateCells();
+            }
         };
                 
         for (int i=0; i<size; i++){
             for (int j=0; j<size; j++){
             	var pos = new Pair<>(j,i);
                 final JButton jb = new JButton(" ");
-                this.cells.add(jb);
+                this.cells.put(pos, jb);
                 jb.addActionListener(al);
                 panel.add(jb);
             }
         }
         this.setVisible(true);
     }
-    
+    private void updateCells(){
+        var p = logics.getNext();
+        for (var entry  : this.cells.entrySet()) {
+            var optDirection = logics.changeDirectionToLeftAt(entry.getKey().getX(), entry.getKey().getY());
+            entry.getValue().setText(optDirection.map(b -> b ? "L " : "R" ).orElse(" "));
+            if(entry.getKey().equals(p)){
+                entry.getValue().setText("*");
+            }
+        }
+    }
 }
