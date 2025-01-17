596902534eccb50fc472eb60249bbe60eee820bf
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-01-17 10:12:26 +0100

    made Implementation of uniFactory

--- oop2021/a02b/e1/UniversityProgramFactoryImpl.java
+++ oop2021/a02b/e1/UniversityProgramFactoryImpl.java
@@ -1,29 +1,82 @@
 package a02b.e1;
 
+import static a02b.e1.UniversityProgram.Sector.COMPUTER_ENGINEERING;
+import static a02b.e1.UniversityProgram.Sector.COMPUTER_SCIENCE;
+import static a02b.e1.UniversityProgram.Sector.MATHEMATICS;
+import static a02b.e1.UniversityProgram.Sector.PHYSICS;
+import static a02b.e1.UniversityProgram.Sector.THESIS;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+import a02b.e1.UniversityProgram.Sector.*;
+import a02b.e1.UniversityProgram.Sector;
 public class UniversityProgramFactoryImpl implements UniversityProgramFactory{
 
+    private static Pair<Predicate<Sector>, Predicate<Integer>> constraint(Predicate<Sector> p1, Predicate<Integer> p2){
+        return new Pair<>(p1, p2);
+    }
     @Override
     public UniversityProgram flexible() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'flexible'");
+       return new AbstractUniversityProgram() {
+
+        @Override
+        protected Set<Pair<Predicate<Sector>, Predicate<Integer>>> getConstraints() {
+            return Set.of(
+                constraint(sector -> true, credits->credits == 60)
+            );
+        }
+        
+       };
     }
 
     @Override
     public UniversityProgram scientific() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'scientific'");
+        return new AbstractUniversityProgram() {
+
+            @Override
+            protected Set<Pair<Predicate<Sector>, Predicate<Integer>>> getConstraints() {
+                return Set.of(
+                    constraint(sector->true, credits->credits==60),
+                    constraint(sector->sector== MATHEMATICS, credits->credits>=12), 
+                    constraint(sector->sector == COMPUTER_SCIENCE, credits->credits>=12),
+                    constraint(sector -> sector == PHYSICS, credits-> credits >=12)
+                );
+            }
+            
+        };
     }
 
     @Override
     public UniversityProgram shortComputerScience() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'shortComputerScience'");
+       return new AbstractUniversityProgram() {
+
+        @Override
+        protected Set<Pair<Predicate<Sector>, Predicate<Integer>>> getConstraints() {
+            return Set.of(
+                constraint(sector->true, credits-> credits >=48),
+                constraint(sector -> sector == COMPUTER_SCIENCE || sector == COMPUTER_ENGINEERING, credits-> credits >=30)
+            );
+        }
+        
+       };
     }
 
     @Override
     public UniversityProgram realistic() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'realistic'");
+        return new AbstractUniversityProgram(){
+
+            @Override
+            protected Set<Pair<Predicate<Sector>, Predicate<Integer>>> getConstraints() {
+                return Set.of(
+                    constraint(sector -> true, credits -> credits>=120),
+                    constraint(sector -> sector == COMPUTER_ENGINEERING || sector == COMPUTER_SCIENCE, credits -> credits >=60),
+                    constraint(sector-> sector == PHYSICS || sector == MATHEMATICS, credits -> credits <=18), 
+                    constraint(sector -> sector ==THESIS, credits -> credits == 24)
+                );
+            }
+
+        };
     }
 
 }
