674305115e5a23a123ddcec1b8efe552c97add13
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-01-17 09:38:13 +0100

    created UNiProFactImpl

---
+++ oop2021/a02b/e1/UniversityProgramFactoryImpl.java
@@ -0,0 +1,29 @@
+package a02b.e1;
+
+public class UniversityProgramFactoryImpl implements UniversityProgramFactory{
+
+    @Override
+    public UniversityProgram flexible() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'flexible'");
+    }
+
+    @Override
+    public UniversityProgram scientific() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'scientific'");
+    }
+
+    @Override
+    public UniversityProgram shortComputerScience() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'shortComputerScience'");
+    }
+
+    @Override
+    public UniversityProgram realistic() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'realistic'");
+    }
+
+}
