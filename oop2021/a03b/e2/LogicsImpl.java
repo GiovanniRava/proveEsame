7177974db39691962efcb2a0563d9498bddcf8bb
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-01-17 17:29:49 +0100

    created LogicsImpl

---
+++ oop2021/a03b/e2/LogicsImpl.java
@@ -0,0 +1,12 @@
+package a03b.e2;
+
+import java.util.Optional;
+
+public class LogicsImpl implements Logics{
+
+    @Override
+    public Optional<Pair<Integer, Integer>> next() {
+        
+    }
+
+}
