286297326946beacc09429508b3b7358c972e90f
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-01-17 15:14:52 +0100

    made DecisionChainFactoryImpl

--- oop2021/a03a/e1/DecisionChainFactoryImpl.java
+++ oop2021/a03a/e1/DecisionChainFactoryImpl.java
@@ -1,8 +1,6 @@
 package a03a.e1;
 
 import java.util.*;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -56,8 +54,28 @@ public class DecisionChainFactoryImpl implements DecisionChainFactory{
 
     @Override
     public <A, B> DecisionChain<A, B> switchChain(List<Pair<Predicate<A>, B>> cases, B defaultReply) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'switchChain'");
+        return new DecisionChain<A,B>() {
+
+            @Override
+            public Optional<B> result(A a) {
+                if(cases.isEmpty()){
+                    return Optional.of(defaultReply);
+                }
+                if(cases.get(0).get1().test(a)){
+                    return Optional.of(cases.get(0).get2());
+                }
+                return Optional.empty();
+            }
+
+            @Override
+            public DecisionChain<A, B> next(A a) {
+                
+                var recursiveCases = new LinkedList<>(cases);
+                recursiveCases.poll();
+                return switchChain(recursiveCases, defaultReply);
+            }
+            
+        };
     }
 
 }
