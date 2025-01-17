f055250eda53a9f8613a3970818463c121be0b16
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-01-17 11:37:29 +0100

    made the Logics implementation

--- oop2021/a02b/e2/Logics.java
+++ oop2021/a02b/e2/Logics.java
@@ -1,11 +1,15 @@
 package a02b.e2;
 import java.util.Optional;
 public interface Logics {
+    enum Direction{
+        LEFT, RIGHT, UP;
+    }
+    
     void toNext();
 
     Pair<Integer, Integer> getNext();
 
     Optional<Boolean> changeDirectionToLeftAt(int x, int y);
-    
+
     boolean isOver();
 }
