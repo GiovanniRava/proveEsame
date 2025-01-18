9bdfc6e6b0120ba0d93593643c24b3aff5b6e492
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-01-18 10:31:20 +0100

    made the GUI

--- oop2021/a03c/e2/GUI.java
+++ oop2021/a03c/e2/GUI.java
@@ -2,17 +2,17 @@ package a03c.e2;
 
 import javax.swing.*;
 import java.util.*;
-import java.util.List;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class GUI extends JFrame {
     
     private static final long serialVersionUID = -6218820567019985015L;
-    private final List<JButton> cells = new ArrayList<>();
-    private int counter = 0;
+    private final Map<Pair<Integer, Integer>, JButton> cells = new HashMap<>();
+    private Logics logics;
     
     public GUI(int size) {
+        this.logics = new LogicsImpl(size);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(50*size, 50*size);
         
@@ -20,19 +20,33 @@ public class GUI extends JFrame {
         this.getContentPane().add(panel);
         
         ActionListener al = e -> {
-        	this.cells.get(counter).setText(String.valueOf(counter++));
+        	var p = logics.next();
+            if(logics.isOver()){
+                System.exit(0);
+            }else {
+                this.updateCells(p);
+            }
         };
                 
         for (int i=0; i<size; i++){
             for (int j=0; j<size; j++){
             	var pos = new Pair<>(j,i);
                 final JButton jb = new JButton(" ");
-                this.cells.add(jb);
+                this.cells.put(pos, jb);
                 jb.addActionListener(al);
                 panel.add(jb);
             }
         }
+        this.updateCells(logics.next());
         this.setVisible(true);
+    }
+    private void updateCells(Set<Pair<Integer, Integer>> p ){
+        for (var entry  : this.cells.entrySet()) {
+            entry.getValue().setText(logics.obstacleAt(entry.getKey().getX(), entry.getKey().getY()) ? "O" : " ");
+            if(p.contains(entry.getKey())){
+                entry.getValue().setText("*");
+            }
+        }
     }
     
 }
