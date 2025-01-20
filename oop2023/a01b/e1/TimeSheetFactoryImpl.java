ab9dad6ba9d0bd28159055cab48e755d5f72a4b5
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-01-20 15:26:23 +0100

    created TimeSheetData record

--- oop2023/a01b/e1/TimeSheetFactoryImpl.java
+++ oop2023/a01b/e1/TimeSheetFactoryImpl.java
@@ -3,29 +3,36 @@ package a01b.e1;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class TimeSheetFactoryImpl implements TimeSheetFactory{
     public static record TimeSheetData(List<String> activities, List<String> days, BiFunction<String, String, Integer> function) implements TimeSheet{
 
         @Override
         public int getSingleData(String activity, String day) {
-            // TODO Auto-generated method stub
-            throw new UnsupportedOperationException("Unimplemented method 'getSingleData'");
+            return activities.contains(activity) && days.contains(day)? function.apply(activity, day) : 0;
         }
 
         @Override
         public Map<String, Integer> sumsPerActivity() {
-            // TODO Auto-generated method stub
-            throw new UnsupportedOperationException("Unimplemented method 'sumsPerActivity'");
+            return activities.stream()
+            .map(act-> new Pair<>(act, days.stream()
+                    .map(day->function.apply(act, day))
+                    .collect(Collectors.summingInt(i->i))))
+            .collect(Collectors.toMap(Pair :: get1, Pair :: get2));
         }
 
         @Override
         public Map<String, Integer> sumsPerDay() {
-            // TODO Auto-generated method stub
-            throw new UnsupportedOperationException("Unimplemented method 'sumsPerDay'");
+            return days.stream()
+                    .map(day -> new Pair<>(day,activities.stream()
+                            .map(act -> fun.apply(act,day))
+                            .collect(Collectors.summingInt(i->i))))
+                    .collect(Collectors.toMap(Pair::get1, Pair::get2));
         }
     }
-    
+
     @Override
     public TimeSheet flat(int numActivities, int numNames, int hours) {
         // TODO Auto-generated method stub
