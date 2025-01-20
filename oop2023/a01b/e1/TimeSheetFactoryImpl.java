154d68f62c6c425dcd840d9d78722980f90df603
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-01-20 15:39:41 +0100

    implemented TimeSheetImpl

--- oop2023/a01b/e1/TimeSheetFactoryImpl.java
+++ oop2023/a01b/e1/TimeSheetFactoryImpl.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TimeSheetFactoryImpl implements TimeSheetFactory{
     public static record TimeSheetData(List<String> activities, List<String> days, BiFunction<String, String, Integer> function) implements TimeSheet{
@@ -32,29 +33,40 @@ public class TimeSheetFactoryImpl implements TimeSheetFactory{
                     .collect(Collectors.toMap(Pair::get1, Pair::get2));
         }
     }
+    private List<String> createActivities(int numActivities){
+        return Stream.iterate(1, i->i+1).map(i->"act"+i).limit(numActivities).collect(Collectors.toList());
+    }
 
+    private List<String> createDays (int numDays){
+        return Stream.iterate(1, i->i+1).map(i->"days"+i).limit(numDays).collect(Collectors.toList());
+    }
     @Override
     public TimeSheet flat(int numActivities, int numNames, int hours) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'flat'");
+        var activities = createActivities(numActivities);
+        var days = createDays(numNames);
+        return new TimeSheetData(activities, days, (a,d)->hours);
     }
 
     @Override
     public TimeSheet ofListsOfLists(List<String> activities, List<String> days, List<List<Integer>> data) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'ofListsOfLists'");
+        return new TimeSheetData(
+            List.copyOf(activities),
+            List.copyOf(days), 
+            (a,d) -> (int)data.get(activities.indexOf(a)).get(data.indexOf(d)));
     }
 
     @Override
     public TimeSheet ofRawData(int numActivities, int numDays, List<Pair<Integer, Integer>> data) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'ofRawData'");
+        var activities = createActivities(numActivities);
+        var days = createDays(numDays);
+        return new TimeSheetData(activities, days, 
+        (a,d)->(int) data.stream().filter(p->p.get1().equals(activities.indexOf(a))&& p.get2().equals(days.indexOf(d))).count());
     }
 
     @Override
     public TimeSheet ofPartialMap(List<String> activities, List<String> days, Map<Pair<String, String>, Integer> data) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'ofPartialMap'");
+        return new TimeSheetData(List.copyOf(activities), List.copyOf(days), 
+        (a,d)-> data.getOrDefault(new Pair<>(a, d), 0));
     }
 
 }
