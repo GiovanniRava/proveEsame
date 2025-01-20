84d19ca3fec30b924a1d8adb34bec006890714e3
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-01-20 17:19:50 +0100

    made GUi

--- oop2023/a01c/e2/GUI.java
+++ oop2023/a01c/e2/GUI.java
@@ -9,9 +9,10 @@ import java.awt.event.ActionListener;
 public class GUI extends JFrame {
     
     private static final long serialVersionUID = -6218820567019985015L;
-    private final List<JButton> cells = new ArrayList<>();
-    
+    private final Map<JButton, Position> cells = new HashMap<>();
+    private Logic logic;
     public GUI(int size) {
+        this.logic = new LogicImpl(size);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(100*size, 100*size);
         
@@ -20,14 +21,25 @@ public class GUI extends JFrame {
         
         ActionListener al = e -> {
             var jb = (JButton)e.getSource();
-        	jb.setText(String.valueOf(cells.indexOf(jb)));
+        	this.logic.hit(this.cells.get(jb));
+            for (var entry : this.cells.entrySet()) {
+                entry.getKey().setText(
+                    this.logic
+                        .getMark(entry.getValue())
+                        .map(String::valueOf)
+                        .orElse("")  
+                );
+            }
+            if(this.logic.isOver()){
+                System.exit(0);
+            }
         };
                 
         for (int i=0; i<size; i++){
             for (int j=0; j<size; j++){
-            	var pos = new Pair<>(j,i);
+            	var pos = new Position(j,i);
                 final JButton jb = new JButton(pos.toString());
-                this.cells.add(jb);
+                this.cells.put(jb, pos);
                 jb.addActionListener(al);
                 panel.add(jb);
             }
