848c5523912e9f0ba818f11d0f9d69ef4e1ae80d
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-01-21 12:26:17 +0100

    made logic impl

--- oop2023/a02b/e2/Position.java
+++ oop2023/a02b/e2/Position.java
@@ -1,5 +1,19 @@
 package a02b.e2;
 
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public record Position(int x, int y) {
+    public Position delta(Position p){
+        return new Position(x - p.x(), y - p.y());
+    }
+
+    public boolean holdInBoth(Predicate<Integer> pred){
+        return pred.test(x) && pred.test(y);
+    }
+
+    public Position applyToBoth(Function<Integer, Integer> fun){
+        return new Position(fun.apply(x), fun.apply(y));
+    }
 
 }
