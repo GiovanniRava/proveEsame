c1d8a16af7828a781f0f969ec015d57acdb349a0
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-01-21 19:24:08 +0100

    made the BankAccountIMpl

---
+++ oop2023/a05/e1/BankAccountFactoryImpl.java
@@ -0,0 +1,162 @@
+package a05.e1;
+
+public class BankAccountFactoryImpl implements BankAccountFactory {
+
+    @Override
+    public BankAccount simple() {
+       return new AbstractBankAccount(){
+
+        @Override
+        protected boolean canDeposit(int amount) {
+           return true;
+        }
+
+        @Override
+        protected void onDisallowedDeposit() {
+        }
+
+        @Override
+        protected void onDisallowedWithdraw() {
+        }
+
+        @Override
+        protected int newBalanceOnWithdraw(int amount) {
+            return balance()-amount;
+        }
+
+        @Override
+        protected boolean canWithdraw(int amount) {
+            return balance()>= amount;
+        }
+       };
+    }
+
+    @Override
+    public BankAccount withFee(int fee) {
+        return new AbstractBankAccount(){
+
+            @Override
+            protected boolean canDeposit(int amount) {
+                return true;
+            }
+
+            @Override
+            protected void onDisallowedDeposit() {
+            }
+
+            @Override
+            protected void onDisallowedWithdraw() {
+                
+            }
+
+            @Override
+            protected int newBalanceOnWithdraw(int amount) {
+                return balance() - amount - fee;
+            }
+
+            @Override
+            protected boolean canWithdraw(int amount) {
+                return balance() >= amount+fee;
+            }
+
+        };
+    }
+
+    @Override
+    public BankAccount checked() {
+        return new AbstractBankAccount(){
+
+            @Override
+            protected boolean canDeposit(int amount) {
+                if(amount <0 ){
+                    return false;
+                }
+                return true;
+            }
+
+            @Override
+            protected void onDisallowedDeposit() {
+                throw new IllegalStateException();
+            }
+
+            @Override
+            protected void onDisallowedWithdraw() {
+                throw new IllegalStateException();
+            }
+
+            @Override
+            protected int newBalanceOnWithdraw(int amount) {
+                return balance()-amount;
+            }
+
+            @Override
+            protected boolean canWithdraw(int amount) {
+                return balance()>= amount && amount >0;
+            }
+        };
+    }
+
+    @Override
+    public BankAccount gettingBlocked() {
+        
+        return new AbstractBankAccount(){
+            private boolean blocked = false;
+            @Override
+            protected boolean canDeposit(int amount) {
+                if(blocked||amount<0){
+                    blocked = true;
+                    return false;
+                }
+                return true;
+            }
+
+            @Override
+            protected void onDisallowedDeposit() {               
+            }
+
+            @Override
+            protected void onDisallowedWithdraw() {
+            }
+
+            @Override
+            protected int newBalanceOnWithdraw(int amount) {
+                return balance()-amount;
+            }
+
+            @Override
+            protected boolean canWithdraw(int amount) {
+                if(blocked|| amount<0 || balance()<amount){
+                    blocked = true;
+                }
+                return !blocked;
+            }
+        };
+    }
+
+    @Override
+    public BankAccount pool(BankAccount primary, BankAccount secondary) {
+        return new BankAccount() {
+
+            @Override
+            public int balance() {
+                return primary.balance()+ secondary.balance();
+            }
+
+            @Override
+            public void deposit(int amount) {
+               if(primary.balance()<=secondary.balance()){
+                    primary.deposit(amount);
+               }else{
+                    secondary.deposit(amount);
+               }
+            }
+
+            @Override
+            public boolean withdraw(int amount) {
+                return primary.withdraw(amount) || secondary.withdraw(amount);
+            }
+            
+        };
+    }
+
+}
