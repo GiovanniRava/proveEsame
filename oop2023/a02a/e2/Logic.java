853a23aea7267c51b7d4034881548159a64259f8
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2025-01-21 11:17:11 +0100

    made logic interface and the record for positions

---
+++ oop2023/a02a/e2/Logic.java
@@ -0,0 +1,10 @@
+package a02a.e2;
+
+public interface Logic {
+    static enum CellType{
+        STATIC, DYNAMIC, EMPTY;
+    }
+    boolean hit(Position position);
+    CellType getMark(Position position);
+    boolean isOver();
+}
